f03b3228aee59a5367621d4a858694f1cc9dcbc8
matt360 <rosasmatias903@gmail.com>
2023-04-12 17:32:36 -0300

    patatatatatapa

--- hola_mundo.java
+++ hola_mundo.java
@@ -1,5 +1,5 @@
-class hola_mundo.java {
-	public static void main(String args[]) {
-		System.out.println("hola mundo");
+public class hola_mundo.java {
+	public static void main(String[] args) {
+	  System.out.println("hola, " + args[0] + " mundo");
 	}
 }
